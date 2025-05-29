d55ef7963f67fc19d445875f75547c5ca32b7520
yuqingcai <yuqingcai@gmail.com>
2025-05-29 16:35:09 +0800

    update

---
+++ detectron2test.py
@@ -0,0 +1,37 @@
+import cv2
+import torch
+import numpy as np
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+# 设置配置
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file(
+    "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # 置信度阈值
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+    "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+)
+cfg.MODEL.DEVICE = "cpu"
+# 创建预测器
+predictor = DefaultPredictor(cfg)
+
+# 读取图片
+image_path = "./data-unversioned/IMG_7857.jpeg"
+image = cv2.imread(image_path)
+
+# 推理
+outputs = predictor(image)
+
+# 可视化
+v = Visualizer(image[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]))
+out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+
+# 显示结果
+cv2.imshow("Result", out.get_image()[:, :, ::-1])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
