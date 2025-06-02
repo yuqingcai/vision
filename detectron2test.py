de48564acc025d27a563918e6a9c38527ff88309
yuqingcai <yuqingcai@gmail.com>
2025-06-02 15:23:37 +0800

    update

--- detectron2test.py
+++ detectron2test.py
@@ -2,38 +2,56 @@ import cv2
 import torch
 from PIL import Image
 import numpy as np
+import detectron2
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2 import model_zoo
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
+import torchvision
 
-# 设置配置
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file(
-    "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
-))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # 置信度阈值
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
-    "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
-)
-cfg.MODEL.DEVICE = "cuda"
-# 创建预测器
-predictor = DefaultPredictor(cfg)
 
-# 读取图片
-image_path = "./data-unversioned/input2.jpg"
-image = cv2.imread(image_path)
+def detect_objects():
+    # 设置配置
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file(
+        "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+    ))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # 置信度阈值
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+        "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+    )
+    cfg.MODEL.DEVICE = "cuda"
+    # 创建预测器
+    predictor = DefaultPredictor(cfg)
 
-# 推理
-outputs = predictor(image)
+    # 读取图片
+    image_path = "./data/coco2017/test2017/000000000001.jpg"
+    image = cv2.imread(image_path)
 
-# 可视化
-v = Visualizer(image[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]))
-out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    # 推理
+    outputs = predictor(image)
 
-# 显示结果
-cv2.imshow("Result", out.get_image()[:, :, ::-1])
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # 可视化
+    v = Visualizer(image[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]))
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+
+    # 显示结果
+    cv2.imshow("Result", out.get_image()[:, :, ::-1])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def show_version():
+    print(torch.cuda.is_available())
+    print(torch.cuda.device_count())
+    print(torch.cuda.get_device_name(0))
+    print(detectron2.__version__)
+    print(detectron2._C)
+    print(torchvision.__version__)
+
+
+if __name__ == "__main__":
+    show_version()
+    detect_objects()
+
 
---
+++ detectron2train.py
@@ -0,0 +1,66 @@
+import os
+import math
+from detectron2.data.datasets import register_coco_instances
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+from pycocotools.coco import COCO
+
+if __name__ == "__main__":
+    annotations_file = './data/coco2017/annotations/instances_train2017.json'
+    image_files = './data/coco2017/train2017'
+    register_coco_instances("train_dataset", 
+        {}, 
+        annotations_file, 
+        image_files)
+
+    coco = COCO(annotations_file)
+
+    # 计算迭代次数 max_iter，detectron2 的迭代次数是指所有 epoch 的迭代次数。
+    # 例如，假设每个 epoch 有 1000 个 batch，epoch 数为 10，则 max_iter = 1000 * 10 = 10000。
+    # 
+    # 在深度学习训练中，最后一个 batch 不足 batch size 是很常见的情况。大多数
+    # 框架（包括 Detectron2 和 PyTorch）会自动处理最后一个 batch 的图片数少于
+    # 你设置的 IMS_PER_BATCH（即 batch size）的情况。
+    # 
+    image_ids = coco.getImgIds()
+    items_per_batch = 2
+    epoch = 10
+    max_iter = math.ceil(len(image_ids) / items_per_batch) * epoch
+
+    print(f'image num: {len(image_ids)}')
+    print(f'items per batch: {items_per_batch}')
+    print(f'epoch num: {epoch}')
+    print(f'iteration num: {max_iter}')
+
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file(
+        "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+    ))
+    cfg.DATASETS.TRAIN = ("train_dataset",)
+    cfg.DATASETS.TEST = ()
+    cfg.DATALOADER.NUM_WORKERS = 2
+
+    # 这里初始化模型权重，事实上mask_rcnn_R_50_FPN_3x.yaml文件中已经指定
+    # 使用detectron2://ImageNetPretrained/MSRA/R-50.pkl初始化模型权重，
+    # 但这个权重只给 backbone（ResNet-50 主干部分），不包括 FPN 和 
+    # Mask R-CNN 头部。
+    # 这里的初始化包括了 backbone， FPN 和 Mask R-CNN 头部。
+    # 
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+        "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+    )
+    
+    cfg.SOLVER.IMS_PER_BATCH = items_per_batch
+    cfg.SOLVER.MAX_ITER = max_iter
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+    cfg.SOLVER.BASE_LR = 0.00025
+
+     # COCO类别数，如自定义数据集需修改
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 80
+
+    cfg.OUTPUT_DIR = "./imagerec/output"
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+    trainer = DefaultTrainer(cfg)
+    trainer.resume_or_load(resume=False)
+    trainer.train()
