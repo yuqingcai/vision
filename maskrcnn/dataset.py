7a7f292a499d6482c9c053651065da59beaa53b9
yuqingcai <yuqingcai@gmail.com>
2025-06-17 17:08:07 +0800

    update

--- maskrcnn/dataset.py
+++ maskrcnn/dataset.py
@@ -2,9 +2,8 @@ import tensorflow as tf
 from pycocotools.coco import COCO
 from pycocotools import mask as maskUtils
 import os
-import numpy as np
 import functools
-import json
+import numpy as np
 
 
 def load_image_info(coco, img_ids, img_dir):
@@ -21,10 +20,10 @@ def load_image_info(coco, img_ids, img_dir):
         if not os.path.exists(file_path):
             print(f"Warning: File {file_path} does not exist.")
             continue
-
+        
         if not annotations:
             # If no annotations, skip this image
-            print(f"Warning: No annotations found for {file_path}.")
+            # print(f"Warning: No annotations found for {file_path}.")
             continue
         
         # Collect all bounding boxes and class_ids
@@ -34,95 +33,65 @@ def load_image_info(coco, img_ids, img_dir):
         for annotation in annotations:
             if annotation.get('iscrowd', 0) == 1:
                 # Skip images with crowd annotations
-                print(f"Warning: Skipping crowd annotation in {file_path}.")
+                # print(f"Warning: Skipping crowd annotation in {file_path}.")
                 continue
             
             if 'bbox' in annotation and 'category_id' \
-                and 'segmentation' in annotation:
+                and 'segmentation' in annotation \
+                and isinstance(annotation['segmentation'], list):
                 
                 # category_id handling
-                category_id = annotation['category_id']
-                category_ids.append(category_id)
+                category_ids.append(annotation['category_id'])
 
                 # bboxes handling
                 x, y, w, h = annotation['bbox']
                 bboxes.append([x, y, x+w, y+h])
 
                 # segmentation handling
-                # Check if segmentation is a polygon or RLE, then make compress
-                segmentation = annotation['segmentation']
-                if isinstance(segmentation, list):
-                    # polygon
-                    rles = maskUtils.frPyObjects(segmentation, height, width)
-                    rle = maskUtils.merge(rles)
-                elif isinstance(segmentation['counts'], list):
-                    # uncompressed
-                    rle = maskUtils.frPyObjects(segmentation, height, width)
-                    rle = maskUtils.merge(rle)
-                else:
-                    # compressed
-                    rle = segmentation
-
-                if isinstance(rle, list):
-                    rle = rle[0]
-                # Ensure counts is a string
-                if isinstance(rle['counts'], bytes):
-                    rle['counts'] = rle['counts'].decode('ascii')
-                rle_str = json.dumps(rle)
-                segmentations.append(rle_str)
-
+                segmentations.append(annotation['segmentation'])
+        
         # If no bounding boxes or category_ids, skip this image
         if not bboxes or not category_ids or not segmentations:
-            print(f"Warning: No bboxes or class_ids or segmentations found for {file_path}.")
+            print(f"Warning: Annotation details not found for {file_path}.")
             continue
         
         if len(bboxes) != len(category_ids) or \
             len(bboxes) != len(segmentations):
-            print(f"Warning: Mismatch in number of bboxes and category_ids, segmentations for {file_path}.")
+            print(f"Warning: Annotation details mismatch in {file_path}.")
             continue
         
         data.append({
-            'file_path': file_path,
+            'file_path' : file_path, 
             'size' :  (height, width), 
             'bboxes' : bboxes,
-            'category_ids': category_ids,
+            'category_ids' : category_ids,
             'segmentations' : segmentations, 
         })
+
     return data
 
 
-def generator(entries):
-    for entry in entries:
-        yield {
-            'file_path': tf.constant(entry['file_path'], dtype=tf.string),
-            'size': tf.constant(entry['size'], dtype=tf.int32),
-            'bboxes': tf.ragged.constant(entry['bboxes'], dtype=tf.float32),
-            'category_ids': tf.constant(entry['category_ids'], dtype=tf.int32),
-            'segmentations': tf.constant(entry['segmentations'], dtype=tf.string),
-        }
-
-
-def size_to(size, min_size=800, max_size=1333):
+def scale_to(size, min_size=800, max_size=1333):
     h = tf.cast(size[0], tf.float32)
     w = tf.cast(size[1], tf.float32)
-    scale = tf.minimum(min_size / tf.minimum(h, w),
-                    max_size / tf.maximum(h, w))
-    new_h = tf.cast(tf.round(tf.cast(h, tf.float32) * \
-                             tf.cast(scale, tf.float32)), tf.int32)
-    new_w = tf.cast(tf.round(tf.cast(w, tf.float32) * \
-                             tf.cast(scale, tf.float32)), tf.int32)
+    scale = tf.minimum(min_size / tf.minimum(h, w), \
+                max_size / tf.maximum(h, w))
+    return scale
+
+
+def resize_to(size, scale):
+    h = tf.cast(size[0], tf.float32)
+    w = tf.cast(size[1], tf.float32)
+    new_h = tf.cast(tf.round(h * tf.cast(scale, tf.float32)), tf.int32)
+    new_w = tf.cast(tf.round(w * tf.cast(scale, tf.float32)), tf.int32)
     return tf.stack([new_h, new_w])
 
 
-
-def resize_image(image, min_size=800, max_size=1333):
+def resize_image(image, scale):
     h = tf.shape(image)[0]
     w = tf.shape(image)[1]
-    
-    new_hw = size_to((h, w), min_size, max_size)
-    new_h = new_hw[0]
-    new_w = new_hw[1]
-    resized = tf.image.resize(image, (new_h, new_w), method='bilinear')
+    new_hw = resize_to(tf.stack([h, w]), scale)
+    resized = tf.image.resize(image, new_hw, method='bilinear')
     return resized
 
 
@@ -148,34 +117,115 @@ def max_size_in_batch(sizes):
     return max_height, max_width
 
 
-def load_image(file_path, min_size, max_size, padding_hw):
+def load_image(file_path, scale, padding_hw):
     img_raw = tf.io.read_file(file_path)
     image = tf.image.decode_jpeg(img_raw, channels=3)
     image = tf.image.convert_image_dtype(image, tf.float32)
-    resized = resize_image(image, min_size, max_size)
-    padded = padding_image(resized, height=padding_hw[0], width=padding_hw[1])
+    resized = resize_image(image, scale)
+    padded = padding_image(resized, height=padding_hw[0], 
+                           width=padding_hw[1])
     return padded
+
+
+def resize_bboxes(bboxes, scale):
+    return bboxes * tf.cast(scale, tf.float32)
+
+
+def resize_segmentations(segmentations, scale):
+    return segmentations * tf.cast(scale, tf.float32)
+
+
+def generator(entries):
+    for entry in entries:
+        yield {
+            'file_path': tf.constant(
+                entry['file_path'], 
+                dtype=tf.string
+                ),
+            'size': tf.constant(
+                entry['size'], 
+                dtype=tf.int32
+                ),
+            'bboxes': tf.ragged.constant(
+                entry['bboxes'], 
+                dtype=tf.float32
+                ),
+            'category_ids': tf.constant(
+                entry['category_ids'], 
+                dtype=tf.int32
+                ),
+            'segmentations': tf.ragged.constant(
+                entry['segmentations'], 
+                dtype=tf.float32
+                ),
+        }
+
+
+def polygons_to_mask(segmentations, height, width):
+    rles = maskUtils.frPyObjects(segmentations, height, width)
+    merged_rle = maskUtils.merge(rles)
+    mask = maskUtils.decode(merged_rle)
+    if len(mask.shape) == 3:
+        mask = np.max(mask, axis=2)
+    return mask
 
 
 def preprocess(batch):
     min_size = 800
     max_size = 1333
     
+    scales = tf.map_fn(
+        lambda size: scale_to(size, min_size, max_size),
+        elems=batch['size'],
+        fn_output_signature=tf.TensorSpec(
+            shape=(), 
+            dtype=tf.float32
+        )
+    )
+    
     resizes = tf.map_fn(
-        lambda size: size_to(size, min_size, max_size), 
-        batch['size'],
-        fn_output_signature=tf.TensorSpec(shape=(2,), 
-                                          dtype=tf.int32))
+        lambda args: resize_to(args[0], args[1]),
+        (batch['size'], scales),
+        fn_output_signature=tf.TensorSpec(
+            shape=(2,), 
+            dtype=tf.int32
+        )
+    )
     
     padding_hw = max_size_in_batch(resizes)
 
     images = tf.map_fn(
-        lambda file_path: load_image(file_path, 
-            min_size, max_size, padding_hw), 
-        batch['file_path'],
-        fn_output_signature=tf.TensorSpec(shape=(None, None, 3), 
-                                          dtype=tf.float32))
-    batch['images'] = images
+        lambda args: load_image(args[0], args[1], padding_hw), 
+        (batch['file_path'], scales),
+        fn_output_signature=tf.TensorSpec(
+            shape=(None, None, 3), 
+            dtype=tf.float32
+        )
+    )
+
+    bboxes_resized = tf.map_fn(
+        lambda args: resize_bboxes(args[0], args[1]), 
+        (batch['bboxes'], scales),
+        fn_output_signature=tf.RaggedTensorSpec(
+            shape=(None, 4), 
+            dtype=tf.float32
+        )
+    )
+
+    segmentations_resized = tf.map_fn(
+        lambda args: resize_segmentations(args[0], args[1]),
+        (batch['segmentations'], scales),
+        fn_output_signature=tf.RaggedTensorSpec(
+            shape=(None, None, None), 
+            dtype=tf.float32
+        )
+    )
+
+    batch['image'] = images
+    batch['origin_size'] = batch['size']
+    batch['size'] = resizes
+    batch['bboxes'] = bboxes_resized
+    batch['segmentations'] = segmentations_resized
     
     return batch
 
@@ -187,7 +237,7 @@ def create_dataset(ann_file, img_dir, batch_size=4):
 
     ds = tf.data.Dataset.from_generator(
         functools.partial(generator, entries), 
-        # 
+
         # file_path 是一个标量，shape=()
         # bboxes 是一个第1维长度不固定，第2维长度固定等于4的tensor,
         # 必须使用 RaggedTensorSpec 进行申明，shape=(None, 4)
@@ -205,13 +255,35 @@ def create_dataset(ann_file, img_dir, batch_size=4):
         # RaggedTensor，对于多维 Tesnsor，如果某一维的长度不固定，
         # 则必须使用 RaggedTensorSpec 对其申明，指出哪个维度是不等
         # 长的。
-        # 
         output_signature={
-            'file_path': tf.TensorSpec(shape=(), dtype=tf.string),
-            'size': tf.TensorSpec(shape=(2,), dtype=tf.int32), 
-            'bboxes': tf.RaggedTensorSpec(shape=(None, 4), dtype=tf.float32),
-            'category_ids': tf.TensorSpec(shape=(None,), dtype=tf.int32),
-            'segmentations': tf.TensorSpec(shape=(None,), dtype=tf.string),
+            'file_path': tf.TensorSpec(
+                shape=(), 
+                dtype=tf.string
+                ),
+
+            'size': tf.TensorSpec(
+                shape=(2,), 
+                dtype=tf.int32
+                ), 
+
+            'bboxes': tf.RaggedTensorSpec(
+                shape=(None, 4), 
+                dtype=tf.float32
+                ),
+
+            'category_ids': tf.TensorSpec(
+                shape=(None,), 
+                dtype=tf.int32),
+
+            # 一个 segmentation 是一个2维 List，由若干个 polygon 构成，
+            # segmentation = [ [], [], []...]，
+            # pycocotools.mask.merge 用于把多个 polygon 合并成一个。
+            # 每张图片的 segmentations就是 N 个 List，每个 List 的长
+            # 度不固定，shape=(None, None, None)
+            'segmentations': tf.RaggedTensorSpec(
+                shape=(None, None, None), 
+                dtype=tf.float32
+                ),
         })
     
     ds = ds.ragged_batch(batch_size)    
--- maskrcnn/main.py
+++ maskrcnn/main.py
@@ -10,119 +10,73 @@ import os
 import numpy as np
 from model import MaskRCNN
 from dataset import create_dataset
-
+import itertools
+import random
 
 # tf.config.set_visible_devices([], 'GPU')
 
-coco_root = "../../dataset/coco2017/"
-train_img_dir = os.path.join(coco_root, "train2017")
-ann_file = os.path.join(coco_root, "annotations/instances_train2017.json")
+coco_root = '../../dataset/coco2017/'
+train_img_dir = os.path.join(coco_root, 'train2017')
+ann_file = os.path.join(coco_root, 'annotations/instances_train2017.json')
 
 
-if __name__ == "__main__":
+def show_image(sample, i):
+    img = sample['image'][i]
+    size = sample['size'][i]
+    bboxes = sample['bboxes'][i]
+    category_ids = sample['category_ids'][i]
+    segmentations = sample['segmentations'][i]
+
+    plt.figure()
+    plt.imshow(img.numpy())
+    plt.axis('on')
+
+    # size border
+    rect = patches.Rectangle((0, 0), size[1], size[0], 
+                             linewidth=1, 
+                             edgecolor='green',
+                             facecolor='none',
+                             alpha=1.0
+    )
+    plt.gca().add_patch(rect)
+    
+    for bbox, category_id, segmentation in \
+        zip(bboxes, category_ids, segmentations):
+
+        x1, y1, x2, y2 = bbox
+        rect = patches.Rectangle((x1, y1), x2 - x1, 
+                                 y2 - y1, linewidth=1,
+                                 edgecolor='r', 
+                                 facecolor='none', 
+                                 alpha=0.4
+        )
+        plt.gca().add_patch(rect)
+
+        cid = int(category_id.numpy()) if \
+            hasattr(category_id, 'numpy') else int(category_id)
+        plt.gca().text(x1, y1 - 2, str(cid), 
+                        color='yellow', fontsize=6, 
+                        va='bottom',  ha='left', 
+                        bbox=dict(facecolor='black', alpha=0.5, pad=0))
+        
+        color = np.random.rand(3)
+        for polygon in segmentation:
+            polygon = polygon.numpy() if hasattr(polygon, 'numpy') else polygon
+            polygon = polygon.reshape(-1, 2)
+            plt.plot(polygon[:, 0], polygon[:, 1], color='blue', linewidth=1)
+            plt.fill(polygon[:, 0], polygon[:, 1], color=color, alpha=0.5)
+
+
+if __name__ == '__main__':
+    batch_size = 4
     ds_train = create_dataset(
         ann_file=ann_file,
         img_dir=train_img_dir,
-        batch_size=4
+        batch_size=batch_size
     )
-    
-    print(f'walk through batches in dataset...')
-    time_0 = time.time()
-    k = 0
-    for i, data in enumerate(ds_train):
-        # if i == 0:
-        #     for img in data["images"]:
-        #         plt.figure()
-        #         plt.imshow(img.numpy())
-        #         plt.axis('on')
-        #     plt.show()
-            # print(f'first batch image: {data["images"][0].numpy()}')
 
-        print(f'batch {i}:')
-        k += 1
-    print(f'batches: {k}, duration: {time.time() - time_0:.2f} s')
-        # print(f'file_path:')
-        # for image_path in data['file_path']:
-        #     print(f'{image_path.numpy().decode()}')
-
-        # print(f'category_ids:')
-        # for category_id in data['category_ids']:
-        #     print(f'{category_id.numpy().tolist()}')
-
-        # print(f'bboxes:')
-        # for bbox in data['bboxes']:
-        #     print(f'{bbox.numpy().tolist()}')
-
-
-        # print(f'segmentations:')
-        # for segmentation in data['segmentations']:
-        #     print(f'{segmentation.numpy().tolist()}')
-
-        # print(f'')
-
-    # # 选择一张图像 ID（比如第一张）
-    # img_id = image_ids[1]
-    # # 获取图像信息
-    # img_info = coco.loadImgs(img_id)[0]
-    # print("图像信息：", img_info)
-    # # 获取该图像的所有标注 ID
-    # ann_ids = coco.getAnnIds(imgIds=img_id)
-    # # 加载所有标注
-    # anns = coco.loadAnns(ann_ids)
-    # # 遍历打印标注信息
-    # for i, ann in enumerate(anns):
-    #     print(f'ann{i}:')
-    #     print("类别ID:", ann['category_id'])
-    #     print("边框: ", ann['bbox'])  # 格式为 [x, y, width, height]
-    #     print("分割: ", ann.get('segmentation'))  # 可能是 polygon 或 RLE
-    #     print("是否是 crowd:", ann.get('iscrowd', 0))
-
-    # # 假设 anns 是 COCO 的 annotation 列表，img_info 是图片信息
-    # height, width = img_info['height'], img_info['width']
-    # binary_mask = np.zeros((height, width), dtype=np.uint8)
-
-    # for ann in anns:
-    #     segm = ann['segmentation']
-    #     if isinstance(segm, list):  # polygon
-    #         rles = maskUtils.frPyObjects(segm, height, width)
-    #         rle = maskUtils.merge(rles)
-    #     elif isinstance(segm['counts'], list):  # uncompressed RLE
-    #         rle = maskUtils.frPyObjects(segm, height, width)
-    #     else:  # compressed RLE
-    #         rle = segm
-    #     m = maskUtils.decode(rle)
-    #     binary_mask = np.logical_or(binary_mask, m)
-
-    # plt.imshow(binary_mask, cmap='gray')
-    # plt.axis('off')
-    # plt.title('Binary Mask')
-    # plt.show()
-    
-    
-    # img_0, scale_0 = image_tensor('000000000049.jpg')
-    # model = MaskRCNN()
-    # model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4))
-    # model.summary()
-
-    # # output: [logits, bbox_deltas, proposals]
-    # output = model(img_0)
-    # proposals = output[2]
-    # print(proposals.shape)
-
-    # img_np = img_0[0].numpy()  # 去掉 batch 维，转为 numpy
-    # plt.imshow(img_np)
-    # plt.figure(figsize=(12, 12))
-    # plt.imshow(img_np)
-
-    # ax = plt.gca()
-    # for box in proposals.numpy():
-    #     x1, y1, x2, y2 = box
-    #     rect = patches.Rectangle(
-    #         (x1, y1), x2 - x1, y2 - y1,
-    #         linewidth=1, edgecolor='r', facecolor='none', alpha=0.4
-    #     )
-    #     ax.add_patch(rect)
-
-    # plt.title(f"Proposals ({proposals.shape[0]} boxes)")
-    # plt.axis('off')
-    # plt.show()
+    n = random.randint(0, 1000)
+    sample = next(itertools.islice(ds_train, n-1, n))
+    for i in range(batch_size):
+        show_image(sample, i)
+    plt.show()
