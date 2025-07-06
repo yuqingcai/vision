9dfd072f1344a8503066997d91d7a1ddcde0fd58
yuqingcai <yuqingcai@gmail.com>
2025-07-06 19:15:56 +0800

    update

--- maskrcnn/loss_class_spec_box.py
+++ maskrcnn/loss_class_spec_box.py
@@ -36,49 +36,50 @@ def loss_class_spec_box_reg_fn(
         proposals_pos = tf.boolean_mask(proposals_valid, pos_mask)
         if tf.size(proposals_pos) == 0:
             return tf.constant(0.0, dtype=tf.float32)
+        
+        bbox_deltas_pred_pos = tf.boolean_mask(
+            bbox_deltas_pred_valid, 
+            pos_mask
+        )
 
-        # bbox_deltas_pred_pos = tf.boolean_mask(
-        #     bbox_deltas_pred_valid, 
-        #     pos_mask
-        # )
-        # best_gt_inds_pos = tf.boolean_mask(best_gt_inds, pos_mask)# [M]
-        # gt_boxes_pos = tf.gather(gt_bboxes_tensor, best_gt_inds_pos)# [M, 4]
-        # proposals_labels = tf.gather(gt_labels, best_gt_inds_pos)
+        best_gt_inds_pos = tf.boolean_mask(best_gt_inds, pos_mask)      # [M]
+        gt_boxes_pos = tf.gather(gt_bboxes_tensor, best_gt_inds_pos)    # [M, 4]
+        gt_labels_pos = tf.gather(gt_labels, best_gt_inds_pos)
         
         # compute the target deltas
-        px, py, pw, ph = (proposals_pos[:, 0] + proposals_pos[:, 2]) * 0.5, \
-            (proposals_pos[:, 1] + proposals_pos[:, 3]) * 0.5, \
-            proposals_pos[:, 2] - proposals_pos[:, 0], \
-            proposals_pos[:, 3] - proposals_pos[:, 1]
+        # px, py, pw, ph = (proposals_pos[:, 0] + proposals_pos[:, 2]) * 0.5, \
+        #     (proposals_pos[:, 1] + proposals_pos[:, 3]) * 0.5, \
+        #     proposals_pos[:, 2] - proposals_pos[:, 0], \
+        #     proposals_pos[:, 3] - proposals_pos[:, 1]
         
-        gx, gy, gw, gh = (gt_boxes_pos[:, 0] + gt_boxes_pos[:, 2]) * 0.5, \
-            (gt_boxes_pos[:, 1] + gt_boxes_pos[:, 3]) * 0.5, \
-            gt_boxes_pos[:, 2] - gt_boxes_pos[:, 0], \
-            gt_boxes_pos[:, 3] - gt_boxes_pos[:, 1]
+        # gx, gy, gw, gh = (gt_boxes_pos[:, 0] + gt_boxes_pos[:, 2]) * 0.5, \
+        #     (gt_boxes_pos[:, 1] + gt_boxes_pos[:, 3]) * 0.5, \
+        #     gt_boxes_pos[:, 2] - gt_boxes_pos[:, 0], \
+        #     gt_boxes_pos[:, 3] - gt_boxes_pos[:, 1]
         
-        tx = (gx - px) / tf.maximum(pw, 1e-6)
-        ty = (gy - py) / tf.maximum(ph, 1e-6)
-        tw = tf.math.log(gw / tf.maximum(pw, 1e-6))
-        th = tf.math.log(gh / tf.maximum(ph, 1e-6))
+        # tx = (gx - px) / tf.maximum(pw, 1e-6)
+        # ty = (gy - py) / tf.maximum(ph, 1e-6)
+        # tw = tf.math.log(gw / tf.maximum(pw, 1e-6))
+        # th = tf.math.log(gh / tf.maximum(ph, 1e-6))
 
-        target_deltas = tf.stack([tx, ty, tw, th], axis=1)
+        # target_deltas = tf.stack([tx, ty, tw, th], axis=1)
 
-        num_classes = tf.shape(bbox_deltas_pred_pos)[1] // 4
-        bbox_deltas_pred_pos_reshaped = tf.reshape(
-            bbox_deltas_pred_pos, [-1, num_classes, 4])  # [M, C, 4]
+        # num_classes = tf.shape(bbox_deltas_pred_pos)[1] // 4
+        # bbox_deltas_pred_pos_reshaped = tf.reshape(
+        #     bbox_deltas_pred_pos, [-1, num_classes, 4])  # [M, C, 4]
 
-        # 使用 batch gather：对每个样本从对应类别维度提取 delta
-        pred_deltas = tf.gather(
-            bbox_deltas_pred_pos_reshaped, 
-            proposals_labels, 
-            axis=1, 
-            batch_dims=1)  # [M, 4]
+        # # 使用 batch gather：对每个样本从对应类别维度提取 delta
+        # pred_deltas = tf.gather(
+        #     bbox_deltas_pred_pos_reshaped, 
+        #     gt_labels_pos, 
+        #     axis=1, 
+        #     batch_dims=1)  # [M, 4]
 
-        diff = pred_deltas - target_deltas
-        abs_diff = tf.abs(diff)
-        loss = tf.where(abs_diff < 1.0,
-                        0.5 * tf.square(abs_diff),
-                        abs_diff - 0.5)
+        # diff = pred_deltas - target_deltas
+        # abs_diff = tf.abs(diff)
+        # loss = tf.where(abs_diff < 1.0,
+        #                 0.5 * tf.square(abs_diff),
+        #                 abs_diff - 0.5)
         
         return tf.reduce_mean(tf.reduce_sum(loss, axis=1))
     
